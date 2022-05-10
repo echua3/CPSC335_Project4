dbfa84d27d2d08323c652df6d8f4db7b8c16235b
echua3 <epiphany.chua3@gmail.com>
2022-05-09 21:09:53 -0700

    change int variables to floats

--- exhaustive.py
+++ exhaustive.py
@@ -20,25 +20,10 @@ def stock_combinations(items: list) -> list:
             powerset (list): a list with all possible combinations of items
     """
     s = list(items)
-    return itertools.chain.from_iterable(itertools.combinations(s, r) for r in range(len(s)+1))
+    return itertools.chain.from_iterable(itertools.combinations(s, r) 
+        for r in range(len(s)+1))
 
-
-def total_value_items(items: list) -> int:
-    """
-    Function that calculates the total value of the stocks given in items
-
-        parameters -
-            items (list): list of lists [x = number of stocks, y = value]
-
-        return -
-            total (int): total value of all the y inputs in list
-    """
-    total = 0
-    for item in items:
-        total += item[1]
-    return total
-
-def total_value_indices(items: list, indices: list) -> int:
+def total_value(items: list, indices: list) -> float:
     """
     Function that calculates the total value of the stocks given in items with
     the indices listed in indices.
@@ -48,19 +33,19 @@ def total_value_indices(items: list, indices: list) -> int:
             indices (list): list of indices in items
 
         return -
-            total (int): total value of all the y inputs in list
+            total (float): total value of all the y inputs in list
     """
     total = 0
     for i in indices:
         total += items[i][1]
     return total
 
-def verify_combination(M: int, items: list, candidate: list) -> bool:
+def verify_combination(M: float, items: list, candidate: list) -> bool:
     """
     checks if the candidate total value is less than or equal to the maximum
     value M
         parameters -
-            M (int): total available investment sum
+            M (float): total available investment sum
             items (list): list of lists [x = number of stocks, y = value]
             candidate (list): list of indices of values in items
 
@@ -68,18 +53,17 @@ def verify_combination(M: int, items: list, candidate: list) -> bool:
             valid (bool): true if total number is less than or equal to M, false
             otherwise
     """
-    return (total_value_indices(items, candidate) <= M)
+    return (total_value(items, candidate) <= M)
          
 
-
-def stock_maximization(M: int, items: list) -> list:
+def stock_maximization(M: float, items: list) -> list:
     """  
     This function evaluates the number of stocks and value of all possible
     subsets, then selects the subset with the highest value that is still
     under the available fund limit. It recomputes combination at each state.
     
     parameters - 
-        M (int): total available investment sum
+        M (float): total available investment sum
         items (list): list of lists [x = number of stocks, y = value]
     
     return -
@@ -88,12 +72,9 @@ def stock_maximization(M: int, items: list) -> list:
     """
     best = None
     for candidate in stock_combinations(range(len(items))):
-        print(candidate)
         if verify_combination(M, items, candidate):
-            print("valid")
-            if best == None or total_value_indices(items, candidate) > total_value_indices(items, best):
+            if best == None or total_value(items, candidate) > total_value(items, best):
                 best = candidate
-    
     return best
 
 
@@ -101,20 +82,23 @@ def main():
     # check for file input argument
     if len(sys.argv) == 2:
         with open(sys.argv[1], 'r') as file:   
-            max = int(next(file))   # read max from first line
+            max = float(next(file))     # read max from first line
             items = []
-            for line in file:       # read items
+            for line in file:           # read items
                 current_item = line.rstrip('\n').split(", ")
-                items.append(list(map(int, current_item)))
-        print("Items:", items)
-        print("Max:", max)
+                current_item[0] = int(current_item[0])
+                current_item[1] = float(current_item[1])
+                items.append(list(current_item))
         file.close
     else:
         # default values
         max = 10
         items = [[1, 2], [3, 3], [5, 6], [6, 7]]
-
-    print("Best:", stock_maximization(max, items))
+    
+    print("Total Available:", max)
+    print("Items:", items)
+    best_indices =  stock_maximization(max, items)
+    print(total_value(items, best_indices), best_indices)
 
 
 if __name__ == "__main__":
