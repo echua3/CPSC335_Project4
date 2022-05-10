9cc162820d9253393f26c050a7045ccf5b0b5581
echua3 <epiphany.chua3@gmail.com>
2022-05-09 19:10:00 -0700

    get stock combinations using itertools

--- exhaustive.py
+++ exhaustive.py
@@ -8,8 +8,41 @@
 # Project 4: Exhaustive vs. Dynamic Programing
 # Part A: The Exhaustive Search Approach
 import sys
+import itertools
+
+def stock_combinations(items: list) -> list:
+    """ 
+    Function to get the powerset of a set(list), returned as a list.
+    Utilizes the itertools python module to create iterators
+        parameters -
+            items (list): an iterable variable, or list
+        return -
+            powerset (list): a list wit all possible combinations of items
+    """
+    s = list(items)
+    return itertools.chain.from_iterable(itertools.combinations(s, r) for r in range(len(s)+1))
+
+def stock_maximization(M: int, items: list) -> list:
+    """  
+    This function evaluates the number of stocks and value of all possible
+    subsets, then selects the subset with the highest value that is still
+    under the available fund limit. It recomputes combination at each state.
+    
+    parameters - 
+        M (int): total available investment sum
+        items (list): list of lists [ x = number of stocks, y = value]
+    
+    return -
+        best (list): subset of items with the highest value within given limit
+
+    """
+    best = None
+    for candidates in stock_combinations(items):
+        print(candidates)
+
 
 def main():
+    # check for file input argument
     if len(sys.argv) == 2:
         with open(sys.argv[1], 'r') as file:   
             max = int(next(file))   # read max from first line
@@ -21,8 +54,11 @@ def main():
         print("Max:", max)
         file.close
     else:
-        items = [[1, 2], [3, 3], [5, 6], [6, 7]]
+        # default values
         max = 10
+        items = [[1, 2], [3, 3], [5, 6], [6, 7]]
+
+    stock_maximization(max, items)
 
 
 if __name__ == "__main__":
