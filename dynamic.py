0893f2bc807f045586728a34a0ad1394f31e7326
echua3 <epiphany.chua3@gmail.com>
2022-05-09 18:48:10 -0700

    start method

---
+++ dynamic.py
@@ -0,0 +1,15 @@
+# dynamic.py
+# Epiphany Chua
+# echua@csu.fullerton.edu
+# Spring 2022 CPSC 335 (2)
+# Instructor: Dr. Sampson Akwafuo
+# May 9, 2022
+
+# Project 4: Exhaustive vs. Dynamic Programing
+# Part B: The Dynamic Programming Approach
+import sys
+
+def main():
+    pass
+if __name__ == "__main__":
+    main()
